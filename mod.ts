d318d7743fad80cac68629b40964dc03766468b3
m1998 Худий <micha.khudy@gmail.com>
2022-05-13 17:13:47 +0000

    all

--- mod.ts
+++ mod.ts
@@ -1,4 +1,3 @@
-import * as discordeno from "https://deno.land/x/discordeno@13.0.0-rc35/mod.ts";
 export * from "./gateway/mod.ts";
 export * from "./handlers/mod.ts";
 export * from "./helpers/mod.ts";
